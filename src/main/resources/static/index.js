80131193fbb7d1d747510fd35e99b211c9d41bc3
aravindreddyakiti01 <aravindhreddyakiti4579@gmail.com>
2026-07-18 12:07:43 +0530

    toggle user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,27 @@
-// alert("hello from js")
-console.log("hello from js");
+var users=[
+    {
+        "name":"John Doe",
+        "gender":"Male",
+        "img":"john.png"
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"Female",
+        "img":"jane.png"
+    }
+]
+var id=0;
+function ToggleUser()
+{
+    id=(id+1)%2
+    var userImage=document.getElementById("user_image");
+
+    var userName=document.getElementById("user_name");
+    var userGender=document.getElementById("user_gender");
+    userImage.src=users[id].img;
+    userName.innerHTML=users[id].name;
+    userGender.innerHTML=users[id].gender;
+
+
+
+}

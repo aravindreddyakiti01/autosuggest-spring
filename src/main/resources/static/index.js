0edc31e50ad6390092139f035a6222d6266a3740
aravindreddyakiti01 <aravindhreddyakiti4579@gmail.com>
2026-07-19 22:34:22 +0530

    updated

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,50 +1,41 @@
-var users=[
+var users = [
     {
-        "name":"John Doe",
-        "gender":"Male",
-        "img":"john.png"
+        name: "John Doe",
+        gender: "Male",
+        img: "john.png"
     },
     {
-        "name":"Jane Doe",
-        "gender":"Female",
-        "img":"jane.png"
+        name: "Jane Doe",
+        gender: "Female",
+        img: "jane.png"
     }
-]
-var id=0;
-function ToggleUser()
-{
-    id=(id+1)%2
-    var userImage=document.getElementById("user_image");
+];
 
-    var userName=document.getElementById("user_name");
-    var userGender=document.getElementById("user_gender");
-    userImage.src=users[id].img;
-    userName.innerHTML=users[id].name;
-    userGender.innerHTML=users[id].gender;
+var id = 0;
 
+function ToggleUser() {
+    id = (id + 1) % users.length;
+
+    document.getElementById("user_image").src = users[id].img;
+    document.getElementById("user_name").innerHTML = users[id].name;
+    document.getElementById("user_gender").innerHTML = users[id].gender;
 }
-function RandomUser()
-{
+
+function RandomUser() {
     fetch("https://randomuser.me/api/")
-        .then(function(response){
+        .then(function(response) {
             return response.json();
+        })
+        .then(function(data) {
+            var userdata = data.results[0];
 
-    })
-  .then(function(data)
-{
-    var userdata=data.results[0];
-    var userImage=document.getElementById("user_image");
-
-    var userName=document.getElementById("user_name");
-    var userGender=document.getElementById("user_gender");
-    userImage.src=userdata.picture.large;
-    userName.innerHTML=userdata.name.first+" "+userdata.name.last;
-    userGender.innerHTML=userdata.gender;
-})
-.catch(function(err)
-{
-    console.log(err);
-})
-
-
+            document.getElementById("user_image").src = userdata.picture.large;
+            document.getElementById("user_name").innerHTML =
+                userdata.name.first + " " + userdata.name.last;
+            document.getElementById("user_gender").innerHTML =
+                userdata.gender;
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
